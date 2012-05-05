d1094adc6377c65dc673135e712eb4d619223288
Alex Annese <alex@yammer-inc.com>
2012-05-04 17:09:29 -0700

    HEY GUYS, IT'S MY TURN NOW

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uberaddicts::Application.config.secret_token = '5cffeecc6b2a554b383c7db3e3ca304bba5e0e6811159fe17588b4cf26aa4c4d26ab62cb85e6ea3ff7028668f167bec22d8823a1cb57e6bc56e02d712c091d05'
